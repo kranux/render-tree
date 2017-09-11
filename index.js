68c2f0857e871d1acbe2f09495e2ebe7b81a53c6
andrius.kripaitis <andrius.kripaitis@gmail.com>
2017-09-11 10:25:35 +0300

    Add child implemented;

--- index.js
+++ index.js
@@ -27,28 +27,49 @@ const data = [
 }
 ];
 
-const treeContainer = document.getElementById('tree-container');
+const recursiveTreeContainer = document.getElementById('recursive-tree-container');
 
-function renderNode(data, level) {
+function renderNode(container, element, level) {
     let resultText = '';
     for(let i = 0; i<level; i+=1) {
         resultText += '-';
     }
-    resultText = `${resultText} ${data}`;
-    const child = document.createElement('li');
-    child.appendChild(document.createTextNode(resultText));
-    treeContainer.appendChild(child);
+    resultText = `${resultText} ${element.label}`;
+    const listItem = document.createElement('li');
+    listItem.appendChild(document.createTextNode(resultText));
+    const button = document.createElement('a');
+    button.appendChild(document.createTextNode(' +'));
+    button.addEventListener('click', function() {
+        element.childs = element.childs || [];
+        element.childs.push({
+            label: `Child of '${element.label}' \#${element.childs.length + 1}`
+        });
+        render();
+    });
+
+    listItem.appendChild(button);
+    container.appendChild(listItem);
 }
 
-function renderRecursive(data, level) {
+function renderRecursive(container, data, level) {
     data.forEach(function(element) {
-        renderNode(element.label, level);
+        renderNode(container, element, level);
         if (element.childs) {
-            renderRecursive(element.childs, level + 1);
+            renderRecursive(container, element.childs, level + 1);
         }
     });
 }
 
+function emptyNode(node) {
+    while (node.hasChildNodes()) {
+        node.removeChild(node.lastChild);
+    }
+}
+
+function render() {
+    emptyNode(recursiveTreeContainer);
+    renderRecursive(recursiveTreeContainer, data, 0);
+}
 window.addEventListener('load', function() {
-    renderRecursive(data, 0);
+    render();
 });
